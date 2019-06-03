63010d7f3c18ae39e73e0689cd73376e9fcd8077
Генрих Берайлик <>
2019-06-03 17:04:05 +0300

    initial commit

---
+++ OtusDZ1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OtusDZ1
+//
+//  Created by Генрих Берайлик on 03/06/2019.
+//  Copyright © 2019 Генрих Берайлик. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
