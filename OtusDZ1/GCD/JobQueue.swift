2d64db14e0743e5feaa9aaa42d7135679c909ae8
Генрих Берайлик <genrih95@mail.ru>
2019-08-20 20:01:18 +0300

    Added TestProfiler to git

---
+++ OtusDZ1/GCD/JobQueue.swift
@@ -0,0 +1,13 @@
+//
+//  JobQueue.swift
+//  OtusDZ1
+//
+//  Created by Генрих Берайлик on 14/08/2019.
+//  Copyright © 2019 Генрих Берайлик. All rights reserved.
+//
+
+import Foundation
+
+struct JobQueue {
+    
+}
---
+++ OtusDZ1/GCD/JobScheduler.swift
@@ -0,0 +1,12 @@
+//
+//  JobScheduler.swift
+//  
+//
+//  Created by Генрих Берайлик on 14/08/2019.
+//
+
+import Foundation
+
+class JobScheduler {
+    
+}
